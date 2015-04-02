88387a0d675764108ec742ea593d5ae2398f4bde
Brad Trapp <trapp2357@gmail.com>
2015-04-02 14:23:31 +0000

    Corrected role errors"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  rolify :role_cname => 'Admin'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
